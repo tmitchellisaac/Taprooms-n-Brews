3859311e42792c012cadf46beb354eae569a9fbb
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-06 08:42:07 -0700

    Reorganized routes file a little

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,23 @@ Rails.application.routes.draw do
   get "/", to: "welcome#index"
 
   get "/taprooms/new", to: "taprooms#new"
-  post "/taprooms", to: "taprooms#create"
-
   get "/taprooms", to: "taprooms#index"
   get "/taprooms/:id", to: "taprooms#show"
   get "/taprooms/:id/edit", to: "taprooms#edit"
   patch '/taprooms/:id', to: 'taprooms#update'
+  post "/taprooms", to: "taprooms#create"
+  delete "/taprooms/:id", to: "taprooms#destroy"
+
   get "/beers", to: "beers#index"
   get "/beers/:id", to: "beers#show"
   get "/beers/:beer_id/edit", to: "beers#edit"
   patch "/beers/:beer_id", to: "beers#update"
+  delete "/beers/:id", to: "beers#destroy"
 
   get "/taprooms/:taproom_id/beers/new", to: "taproom_beers#new"
   post "/taprooms/:taproom_id/beers", to: "taproom_beers#create"
   get "/taprooms/:taproom_id/beers", to: "taproom_beers#index"
-
   get "/taprooms/:taproom_id/beers/:sort_code" ,to: "taproom_beers#index"
 
-  delete "/taprooms/:id", to: "taprooms#destroy"
-  delete "/beers/:id", to: "beers#destroy"
 
 end
